7c09d51f732650802167164e1d5ea6ac068d2855
Daniel Washington <danielvertigo@hotmail.com>
2021-05-30 13:03:41 -0300

    Files

--- SpellingitOut/ViewController.swift
+++ SpellingitOut/ViewController.swift
@@ -5,6 +5,24 @@
 //  Created by Daniel Washington Ignacio on 30/05/21.
 //
 
+
+
+/*
+ Create a function which takes in a word and spells it out, by consecutively adding letters until the full word is completed.
+
+ Examples
+
+ spelling("bee") ➞ ["b", "be", "bee"]
+
+ spelling("happy") ➞ ["h", "ha", "hap", "happ", "happy"]
+
+ spelling("eagerly") ➞ ["e", "ea", "eag", "eage", "eager", "eagerl", "eagerly"]
+ Notes
+
+ N/A
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +30,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(spelling("bee"))
+        print(spelling("happy"))
+        print(spelling("eagerly"))
     }
 
-
+    
+    func spelling(_ str: String) -> [String] {
+        var resultString: [String] = []
+        var resulCharacter = ""
+        for n in str{
+            resulCharacter += String(n)
+            resultString.append(resulCharacter)
+            }
+        return resultString
+        }
 }
 
